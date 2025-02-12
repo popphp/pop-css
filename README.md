b769c5dc6b153a4171a44c9fdaaba43414c2ea96
Nick Sagona <nick@noladev.com>
2025-02-12 16:22:33 -0600

    Update version

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ Install `pop-css` using Composer.
 Or, require it in your composer.json file
 
     "require": {
-        "popphp/pop-css" : "^2.0.1"
+        "popphp/pop-css" : "^2.0.2"
     }
 
 [Top](#pop-css)
--- src/Exception.php
+++ src/Exception.php
@@ -21,6 +21,6 @@ namespace Pop\Css;
  * @author     Nick Sagona, III <dev@noladev.com>
  * @copyright  Copyright (c) 2009-2025 NOLA Interactive, LLC.
  * @license    https://www.popphp.org/license     New BSD License
- * @version    2.0.1
+ * @version    2.0.2
  */
 class Exception extends \Exception {}
